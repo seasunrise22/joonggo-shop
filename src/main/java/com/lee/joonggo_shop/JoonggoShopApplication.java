dc62b12daf0061ae37610212651e899e45db037b
lee <seasunrise22@gmail.com>
2024-12-09 20:28:46 +0900

    first commit

---
+++ src/main/java/com/lee/joonggo_shop/JoonggoShopApplication.java
@@ -0,0 +1,13 @@
+package com.lee.joonggo_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoonggoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoonggoShopApplication.class, args);
+	}
+
+}
