72a3a7b5eb6d1db53d146045a1716ce58dbd2898
lee <seasunrise22@gmail.com>
2024-12-11 22:58:25 +0900

    daily

--- src/main/java/com/lee/joonggo_shop/entity/Product.java
+++ src/main/java/com/lee/joonggo_shop/entity/Product.java
@@ -8,4 +8,20 @@ public class Product {
 	private String name; // 중고 상품 이름
 	private String description; // 중고 상품 설명
 	private Integer price; // 중고 상품 가격
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public Integer getPrice() {
+		return price;
+	}
 }
